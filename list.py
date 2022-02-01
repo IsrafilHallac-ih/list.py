e2bba74892413b836550ea5bb631038916c10c7a
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2022-02-01 14:35:58 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,27 @@
+from email import message
+
+
+message="Hello Friends My name is İsrafil" .split()
+print(message[-1])
+
+
+my_list=[1,2,3,4]
+my_list1=[1,"a",True,3.5]
+print(my_list)
+
+list1=["Özge","İsrafil"]
+list2=["Ebru","Sezgin","Tomris"]
+my_list2=list1+list2
+print(my_list2)
+print(len(my_list2))
+print(my_list2[2])
+
+userA=["hakan",36]
+userB=["asel",3]
+
+users=[userA,userB]
+print(userA)
+print(userB)
+print(users)
+print(users[0][1])
+
